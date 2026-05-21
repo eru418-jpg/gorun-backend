4fef8f2cbc10a94f3e527412b7244abc0c13122f
eru418-jpg <eru418@gmail.com>
2026-05-22 00:49:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
+// Veritabanı bağlantısı (Hata olsa bile sunucu çökmesin diye .catch ekliyoruz)
+const dbURI = "mongodb+srv://eru418:35Az7847@cluster0.igadjrp.mongodb.net/gorun_db?retryWrites=true&w=majority";
+mongoose.connect(dbURI)
+  .then(() => console.log("Veritabanı bağlandı!"))
+  .catch((err) => console.log("Veritabanı bağlantısı başarısız, yine de sunucu çalışıyor: ", err));
+
 app.get('/', (req, res) => {
-    res.send('Sunucu aktif ve sorunsuz çalışıyor!');
+  res.send('Backend sorunsuz çalışıyor!');
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(Sunucu ${PORT} portunda başarıyla çalışıyor.);
+  console.log(Sunucu ${PORT} portunda yayında.);
 });
