8fbf294ac52040d66f2b86cd4fc0b841cac1a956
eru418-jpg <eru418@gmail.com>
2026-05-22 00:41:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 
-// Sunucunun Render tarafından atanan portu veya 5000'i dinlemesini sağla
-const PORT = process.env.PORT || 5000;
-
 app.get('/', (req, res) => {
-  res.send('Sunucu çalışıyor ve yayında!');
+    res.send('Sunucu aktif ve sorunsuz çalışıyor!');
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Sunucu ${PORT} portunda başarıyla çalışıyor.);
+    console.log(Sunucu ${PORT} portunda başarıyla çalışıyor.);
 });
