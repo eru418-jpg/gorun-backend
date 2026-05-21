3adfc6ceaf6932e29fc24bf37cfbe7df9993ff96
eru418-jpg <eru418@gmail.com>
2026-05-22 01:12:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 1. Render'ın "Canlı mı?" testine cevap ver
 app.get('/', (req, res) => {
     res.send('Sunucu aktif ve yayında!');
 });
 
-// 2. PORT'u Render'dan al, olmazsa 10000 yap
+// En önemli kısım burası:
 const PORT = process.env.PORT || 10000;
 
-// 3. Sunucuyu kalıcı olarak başlat
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(Sunucu ${PORT} portunda yayında.);
+    console.log(Sunucu ${PORT} portunda başarıyla çalışıyor.);
 });
