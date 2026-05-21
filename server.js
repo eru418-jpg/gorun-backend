7dd34c661cd48bad4c73b96b6f379649aa4455d7
eru418-jpg <eru418@gmail.com>
2026-05-22 02:48:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Veritabanı bağlantısı
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log("Veritabanı bağlantısı başarılı!"))
-  .catch((err) => console.log("Hata:", err));
-
 app.get('/', (req, res) => {
-    res.send('Sunucu aktif ve yayında!');
+    res.send('Sunucu çalışıyor!');
 });
 
 const PORT = process.env.PORT || 10000;
-
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(Sunucu ${PORT} portunda başarıyla çalışıyor.);
+    console.log(Sunucu ${PORT} portunda çalışıyor.);
 });
