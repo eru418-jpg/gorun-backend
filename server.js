007250a3d36e13e41493537848116939f59ced76
eru418-jpg <eru418@gmail.com>
2026-05-21 03:41:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-// 1. Render için portu dinamik olarak alıyoruz
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
-// 2. Atlas'taki bağlantı dizesi (Şifreni buraya yaz)
-const MONGO_URI = "mongodb+srv://eru418:35Az7847@cluster0.igadjrp.mongodb.net/gorun_db?retryWrites=true&w=majority";
-
-// 3. Bağlantı ayarları
 mongoose.connect(MONGO_URI)
   .then(() => console.log("BAĞLANTI BAŞARILI: Veritabanı ile el sıkışıldı!"))
   .catch((err) => console.error("BAĞLANTI HATASI: ", err.message));
 
 app.get('/', (req, res) => {
-    res.send('Sunucu çalışıyor ve veritabanına bağlı!');
+  res.send('Sunucu çalışıyor ve veritabanına bağlı!');
 });
 
 app.listen(PORT, () => {
-    console.log(Sunucu${PORT}portunda yayında.);
+  console.log(Sunucu${PORT}portunda yayında.);
 });
