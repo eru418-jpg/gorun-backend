4511e2f4b272dd934522e9409cb2d56e2e24acd6
eru418-jpg <eru418@gmail.com>
2026-05-22 01:21:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Basit bir test yolu
 app.get('/', (req, res) => {
     res.send('Sunucu aktif ve yayında!');
 });
 
-// Render'ın dinamik portunu yakalamak için en güvenli yöntem
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, '0.0.0.0', () => {
