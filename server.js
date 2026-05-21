b1e185c0fa2c61deb5f16dbf76cf8d27107f6faa
eru418-jpg <eru418@gmail.com>
2026-05-21 23:27:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./models/user');
 const app = express();
 
-app.use(express.json());
-
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = "mongodb+srv://eru418:35Az7847@cluster0.igadjrp.mongodb.net/gorun_db?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://eru418:35Az7847@cluster0.igadjrp.mongodb.net/gorun_db?retryWrites=true&w=majority";
 
 mongoose.connect(MONGO_URI)
-  .then(() => console.log("Veritabanı ve Model başarıyla bağlandı!"))
-  .catch((err) => console.error("BAĞLANTI HATASI: ", err.message));
+  .then(() => console.log("Veritabanı bağlantısı başarılı!"))
+  .catch((err) => console.error("Bağlantı hatası: ", err.message));
 
-// Kayıt olma rotası
-app.post('/register', async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(201).send("Kullanıcı başarıyla kaydedildi!");
-  } catch (err) {
-    res.status(400).send("Kayıt hatası: " + err.message);
-  }
+app.get('/', (req, res) => {
+  res.send('Sunucu çalışıyor ve veritabanına bağlı!');
 });
 
-app.get('/', (req, res) => res.send('Yürüme Arkadaşı Backend aktif!'));
-
-app.listen(PORT, () => console.log(Sunucu ${PORT} portunda çalışıyor.));
+app.listen(PORT, () => {
+  console.log(Sunucu ${PORT} portunda yayında.);
+});
