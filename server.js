5102332c74431115b5a92881545b7e35a8538bc2
eru418-jpg <eru418@gmail.com>
2026-05-21 23:56:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const app = express();
 
 // Veritabanı bağlantısı
 const MONGO_URI = "mongodb+srv://eru418:35Az7847@cluster0.igadjrp.mongodb.net/gorun_db?retryWrites=true&w=majority";
-mongoose.connect(MONGO_URI)
-  .then(() => console.log("Veritabanı bağlı!"))
-  .catch((err) => console.error("Bağlantı hatası: ", err));
 
-// Sunucu ayarları
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("Veritabanı bağlantısı başarılı!"))
+  .catch((err) => console.error("Bağlantı hatası: ", err.message));
+
+// Basit bir test rotası
 app.get('/', (req, res) => {
   res.send('Sunucu çalışıyor!');
 });
 
+// Sunucuyu başlat
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(Sunucu ${PORT} portunda yayında.);
