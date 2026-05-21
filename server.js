f91841ee2a8cb543eb6cec40995c986a08c5dacc
eru418-jpg <eru418@gmail.com>
2026-05-22 02:50:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get('/', (req, res) => {
-    res.send('Sunucu çalışıyor!');
+  res.send('Sunucu calisiyor!');
 });
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(Sunucu ${PORT} portunda çalışıyor.);
+  console.log('Sunucu basarili bir sekilde ayakta.');
 });
