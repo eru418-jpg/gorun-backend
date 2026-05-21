6234807f30877c5886e797fcad4f44779f0bfd3d
eru418-jpg <eru418@gmail.com>
2026-05-21 23:14:45 +0300

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  walkingLevel: { type: String, default: 'Başlangıç' }, // Başlangıç, Orta, İleri
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
