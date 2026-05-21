d5e07aebf767e3ed7af4d87f5f56bc994c9d5a29
eru418-jpg <eru418@gmail.com>
2026-05-22 00:57:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Sunucunun çalışıp çalışmadığını test etmek için
 app.get('/', (req, res) => {
     res.send('Sunucu aktif ve yayında!');
 });
 
-// Port hatasını çözmek için en kritik kısım:
+// Render'ın atadığı portu kullan, yoksa 5000'i kullan
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(Sunucu ${PORT} portunda başarıyla çalışıyor.);
 });
