681ec265186af9477ebb4de24c97c945c77dae1b
eru418-jpg <eru418@gmail.com>
2026-05-22 00:59:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(cors());
-app.use(express.json());
+mongoose.connect(dbURI)
+  .then(() => console.log("Veritabanı bağlantısı başarılı!"))
+  .catch((err) => console.log("Veritabanı hatası (sunucu çalışmaya devam ediyor):", err));
 
 app.get('/', (req, res) => {
-    res.send('Sunucu aktif ve yayında!');
+  res.send('Sunucu aktif ve yayında!');
 });
 
-// Render'ın atadığı portu kullan, yoksa 5000'i kullan
+// Render'ın vereceği portu kullan, yoksa 5000'i kullan
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(Sunucu ${PORT} portunda başarıyla çalışıyor.);
+  console.log(Sunucu ${PORT} portunda başarıyla çalışıyor.);
 });
