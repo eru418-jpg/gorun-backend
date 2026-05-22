763f8c22696a436d037fffe71f5d107f9d370de7
eru418-jpg <eru418@gmail.com>
2026-05-22 04:00:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const connectDB = require('./db');
+const User = require('./models/user');
 const app = express();
 
-// Veritabanı bağlantısını başlat
-connectDB();
-
 app.use(express.json());
+
+// Veritabanı bağlantısını en başta bir kez başlatıyoruz
+connectDB(); 
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Sunucu ve veritabanı aktif!');
