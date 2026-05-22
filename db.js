13d1a4087726d26719386ff59dc1075c1b1b5ba3
eru418-jpg <eru418@gmail.com>
2026-05-22 03:38:27 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,27 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+   const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
     await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Veritabanı bağlantısı başarılı!");
+  } catch (err) {
+    console.error("Veritabanı bağlantısı başarısız:", err.message);
+  }
+};
+
+module.exports = connectDB;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Veritabanı bağlantısı başarılı!");
+  } catch (err) {
+    console.error("Veritabanı bağlantısı başarısız:", err.message);
+  }
+};
     console.log("Veritabanı bağlantısı başarılı!");
   } catch (err) {
     console.error("Veritabanı bağlantısı başarısız:", err.message);
