15b9a6076d144c78e85e4edbc36d03c9dfa921d5
eru418-jpg <eru418@gmail.com>
2026-05-24 01:47:56 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const connectDB = require('./db'); // db.js dosyasından bağlantıyı çekiyoruz
-const app = express();
+const mongoose = require('mongoose');
 
-app.use(express.json());
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB bağlantısı başarılı!");
+  } catch (err) {
+    console.error("Veritabanı bağlantısı başarısız:", err.message);
+    process.exit(1);
+  }
+};
 
-// Veritabanı bağlantısını tetikliyoruz
-connectDB();
-
-app.get('/', (req, res) => {
-  res.send('Sunucu ve veritabanı aktif!');
-});
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log("Sunucu ayakta.");
-});
+module.exports = connectDB;
