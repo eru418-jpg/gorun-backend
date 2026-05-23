279537a14c93696e53f5b54b79eaac4c32f27857
eru418-jpg <eru418@gmail.com>
2026-05-24 01:06:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const app = express();
 
 app.use(express.json());
 
-// Veritabanı bağlantısını tek bir noktada başlat
-connectDB(); 
+// Veritabanı bağlantısını başlat
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Sunucu ve veritabanı aktif!');
 });
 
+// Render için kritik port ayarı
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(Sunucu ${PORT} portunda ayakta.);
+  console.log(Sunucu ${PORT} portunda başarıyla dinleniyor.);
 });
