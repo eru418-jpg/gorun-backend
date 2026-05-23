5ff8324fdcbf6fb5f0f85cd6d09428bc649b8d47
eru418-jpg <eru418@gmail.com>
2026-05-24 01:53:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const app = express();
 
 app.use(express.json());
 
-// Veritabanı bağlantısını başlat
-connectDB();
-
 app.get('/', (req, res) => {
-  res.send('Sunucu ve veritabanı aktif!');
+  res.send('Sunucu aktif, veritabanı arka planda bağlanıyor!');
 });
 
+// Render zaman aşımına düşmesin diye ÖNCE portu hemen açıyoruz
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log("Sunucu ayakta.");
+  console.log(Sunucu ayakta. Port: ${PORT});
+  
+  // Sunucu ayağa kalktıktan hemen sonra veritabanı bağlantısını başlatıyoruz
+  connectDB();
 });
